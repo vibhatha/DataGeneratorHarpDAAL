d38fd1516aff7bc7a250c445aa58e0b4a1d3412b
Vibhatha Abeykoon <vlabeyko@t-055.tango.futuresystems.org>
2017-08-09 12:55:22 -0400

    fix:dev: MLR bulk data generator script modification

--- SyntheticDataGeneratorDAAL/scripts/gen_bulk_spark_linreg.sh
+++ SyntheticDataGeneratorDAAL/scripts/gen_bulk_spark_linreg.sh
@@ -1,5 +1,5 @@
-sh scripts/gen_spark_linreg.sh data/ 100000 300
-sh scripts/gen_spark_linreg.sh data/ 500000 300
-sh scripts/gen_spark_linreg.sh data/ 1000000 300
-sh scripts/gen_spark_linreg.sh data/ 5000000 300
-sh scripts/gen_spark_linreg.sh data/ 10000000 300
+sh scripts/gen_spark_linreg.sh spark_data/ 100000 300
+sh scripts/gen_spark_linreg.sh spark_data/ 500000 300
+sh scripts/gen_spark_linreg.sh spark_data/ 1000000 300
+sh scripts/gen_spark_linreg.sh spark_data/ 5000000 300
+sh scripts/gen_spark_linreg.sh spark_data/ 10000000 300
